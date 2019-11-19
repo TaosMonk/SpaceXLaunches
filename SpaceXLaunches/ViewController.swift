1cd670ce87702ef824d397aa3aec124adb93d51e
Vlad Demenchoock <vlad@vlads-macbook-pro.local>
2019-11-19 19:55:25 +0100

    Initial Commit

---
+++ SpaceXLaunches/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceXLaunches
+//
+//  Created by Vlad Demenchoock on 19.11.2019.
+//  Copyright © 2019 Vlad Demenchoock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
